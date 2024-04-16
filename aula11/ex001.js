c9bb88b8a6ae3653afde185f52ca007f7072b3aa
TawanDeodato <tawandeodato94@gmail.com>
2024-04-16 14:12:50 -0300

    aula 10 de javascript

---
+++ aula11/ex001.js
@@ -0,0 +1,8 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 70) {
+    console.log(`Você ultrapassou a velocidade máxima permitida. MULTADO!!`)
+}else{
+    console.log(`Você está na velocidade permitida. Dirija sempre usando sinto de segurança`)
+}
