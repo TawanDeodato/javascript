d37e9fc09db5612ac48bfdb9e844d56e8d46c670
TawanDeodato <tawandeodato94@gmail.com>
2024-04-19 20:06:21 -0300

    finalizando o desafio 05 de javascript

--- desafios/d05/script.js
+++ desafios/d05/script.js
@@ -21,8 +21,40 @@ function inLista(n, l) {
 
 function clicou() {
     if (isNum(num.value) && !inLista(num.value, v)) {
-
+        v.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou não encontrado!')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function final() {
+    if (v.length == 0) {
+        window.alert('Adicione um valor para prosseguir!')
+    } else {
+        let tot = v.length
+        let maior = v[0]
+        let menor = v[0]
+        let soma = 0
+        let media = 0
+        for (let pos in v) {
+            soma += v[pos]
+            if (v[pos] > maior) 
+                maior = v[pos]
+            if (v[pos] < menor)
+                menor = v[pos]
+        }
+        media = soma/tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior número foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor número foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores adicionados é ${media}</p>`
     }
 }
