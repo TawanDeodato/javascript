b206ad4b3c6c2b50654681e1bdaaff6646cd5385
TawanDeodato <tawandeodato94@gmail.com>
2024-04-30 13:21:20 -0300

    Switch case em JavaScript

---
+++ outros estudos/Youtube/CFBCursos/aula14/script.js
@@ -0,0 +1,23 @@
+// Estudos sobre Switch...case:
+
+let semana = 4
+
+switch(semana) {
+    case 1: 
+        console.log("Hoje é segunda-feira!")
+        break
+    case 2:
+        console.log("Hoje é Terça-feira!")
+        break
+    case 3: 
+        console.log("Hoje é Quarta-feira!")
+        break
+    case 4: 
+        console.log("Hoje é Quinta-feira!")
+        break
+    case 5: 
+        console.log("Hoje é Sexta-feira!")
+        break
+    default:
+        console.log("Não sei que dia é hoje :(")
+}
