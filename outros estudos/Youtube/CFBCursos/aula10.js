d4f59a90d9baea1f09e12965ba819ff3be8b94d8
TawanDeodato <tawandeodato94@gmail.com>
2024-04-30 12:29:28 -0300

    Aula de operador ternário JavaScript

---
+++ outros estudos/Youtube/CFBCursos/aula10.js
@@ -0,0 +1,11 @@
+// Estudos sobre operador ternário
+// Sintaxe:  (Teste lógico ? se verdadeiro : se falso)
+
+let num = 10
+let res = (!(num % 2) ? "Par" : "Impar")
+console.log(res)
+
+let num1 = 9
+let num2 = 8
+let res1 = (num1 > num2 ? "Verdadeiro" : "Falso")
+console.log(res1)
