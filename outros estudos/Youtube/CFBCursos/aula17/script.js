19d9525fec7f9f7ccdba9982cf13863e1f937e3c
TawanDeodato <tawandeodato94@gmail.com>
2024-04-30 19:56:18 -0300

    Loop while no JavaScript

---
+++ outros estudos/Youtube/CFBCursos/aula17/script.js
@@ -0,0 +1,21 @@
+// Estudos sobre o loop 'while'
+// Estrutua básica do 'while':
+
+let n = 0
+
+while (n <= 10) {
+    console.log(n)
+    n++
+}
+
+// Fatorial no loop 'while'
+
+let num = 5
+let fat = 1 
+
+while (num >= 1) {
+    fat *= num
+    num--
+}
+
+console.log(fat)
