e88d67a7ef1d731256c84cbb7fe25865e6df54a0
TawanDeodato <tawandeodato94@gmail.com>
2024-05-03 12:30:40 -0300

    Estudos sobre manipulação do DOM com querySelector

--- outros estudos/Youtube/CFBCursos/aula32/script.js
+++ outros estudos/Youtube/CFBCursos/aula32/script.js
@@ -1,15 +1,27 @@
-// Estudos sobre manipulação do DOM com getElementByClassName:
+// Estudos sobre manipulação do DOM com querySelector:
 
-const cursosTodos = [...document.getElementsByClassName("curso")] // Transformando de HTMLcolletction em array.
+const divTodas = [...document.getElementsByTagName("div")]
+const cursoTodos = [...document.getElementsByClassName("curso")] // Transformando de HTMLcolletction em array.
 const cursosC1 = [...document.getElementsByClassName("c1")]
 const cursosC2 = [...document.getElementsByClassName("c2")]
-const cursoEspecial = document.getElementsByClassName("curso")[3]
+const cursoEspecial = document.getElementsById("c1")
 
-console.log(cursosTodos) // Veja pelo console do DevTools
-console.log(cursosC1) // Veja pelo console do DevTools
-console.log(cursosC2) // Veja pelo console do DevTools
-console.log(cursoEspecial)
+const query_div = document.querySelector("div")
+// const query_divTodas = [...document.querySelectorAll("div")] // Retorna somente divs
+const query_divTodas = [...document.querySelectorAll("div, p")] // Retrona divs e parágrafos
+console.log(query_div) // Ele retorna a primeria div que encontrar
+console.log(query_divTodas) // Ele retorna todas as divs
 
-cursosC1.map((el) => {
-    el.classList.add("destaque") // Foi adicionado uma claase destaque das CSS no HTML.
-})
+const query_cursoTodos = [...querySelectorAll(".curso")] // Retorna todos os elementos que usam a classe ".curso"
+const query_cursosC1 = [...document.querySelectorAll("c1")]
+const query_cursosC2 = [...document.querySelectorAll("c2")]
+const query_cursoEspecial = document.querySelector("#c1")
+
+console.log(query_cursosC1)
+console.log(query_cursosC2)
+
+// console.log(divTodas)
+// console.log(cursoTodos) // Veja pelo console do DevTools
+// console.log(cursosC1) // Veja pelo console do DevTools
+// console.log(cursosC2) // Veja pelo console do DevTools
+// console.log(cursoEspecial)
