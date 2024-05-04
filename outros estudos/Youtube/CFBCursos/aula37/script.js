62c664c2cc62ed01c6272b1c082dceeb9a8a0754
TawanDeodato <tawandeodato94@gmail.com>
2024-05-04 12:43:57 -0300

    Estudos do método stopPropagation no JavaScript

---
+++ outros estudos/Youtube/CFBCursos/aula37/script.js
@@ -0,0 +1,15 @@
+// Estudos do método stopPropagation no JavaScript:
+
+const caixa1 = document.querySelector("#caixa1")
+const btn_c1 = document.querySelector("#c1")
+const cursos = [...document.querySelectorAll(".curso")]
+
+// caixa1.addEventListener("click", (evt) => {
+//     console.log("clicou")
+// })
+
+cursos.map((el) => {
+    el.addEventListener("click", (evt) => {
+        evt.stopPropagation()
+    })
+})
