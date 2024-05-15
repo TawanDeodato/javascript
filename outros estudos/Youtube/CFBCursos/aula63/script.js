af88d1c039d3eb1fecf9d1a1ab9e93306228a1e3
TawanDeodato <tawandeodato94@gmail.com>
2024-05-14 21:21:24 -0300

    Parte 2 do exercicio de classes no javascript

--- outros estudos/Youtube/CFBCursos/aula63/script.js
+++ outros estudos/Youtube/CFBCursos/aula63/script.js
@@ -5,11 +5,16 @@ const f_tipoNormal = document.querySelector("#f_tipoNormal")
 const f_blindagem = document.querySelector("#f_blindagem")
 const f_municao = document.querySelector("#f_municao")
 const carros = document.querySelector("#carros")
-const btn_addCarro = document.querySelector("#bnt_addCarro")
 const f_nome = document.querySelector("#f_nome")
 const f_portas = document.querySelector("#f_portas")
+const btn_addCarro = document.querySelector("#btn_addCarro")
 
 let a_carros = []
+const removerCarro= (quem) => {
+    a_carros = a_carros.filter((el) => {
+        return el.nome != quem
+    })
+}
 
 f_tipoMilitar.addEventListener("click", (evt) => {
     f_nome.value = ""
@@ -32,25 +37,35 @@ const gerenciarExibicaoCarros = () => {
     carros.innerHTML = ""
     a_carros.forEach((c) => {
         const div = document.createElement("div")
+        const btn = document.createElement("button")
+        btn.addEventListener("click", (evt) => {
+            const quemRemover = evt.target.parentNode.dataset.nome
+            removerCarro(quemRemover)
+            gerenciarExibicaoCarros()
+        })
         div.setAttribute("class", "carro")
+        div.setAttribute("data-nome", c.nome)
         div.innerHTML = `Nome: ${c.nome}</br>`
         div.innerHTML += `Portas: ${c.portas}</br>`
         div.innerHTML += `Blindagem: ${c.blindagem}</br>`
         div.innerHTML += `Munição: ${c.municao}</br>`
         div.innerHTML += `Cor: ${c.cor}`
+        btn.innerHTML = "Remover"
+        div.appendChild(btn)
         carros.appendChild(div)
     })
 }
-
-btn_addCarro.addEventListener("click", () => {
-    if (f_tipoNormal.checked) {
-        const c = new Carro (f_nome.value, f_portas.value)
-        a_carros.push(c)
-    } else {
-        const c = new Militar (f_nome.value, f_portas.value, f_blindagem.value, f_municao.value)
-        a_carros.push(c)
-    }
-    gerenciarExibicaoCarros()
+document.addEventListener("DOMContentLoaded", () => {
+    btn_addCarro.addEventListener ("click", () => {
+        if (f_tipoNormal.checked) {
+            const c = new Carro (f_nome.value, f_portas.value)
+            a_carros.push(c)
+        } else {
+            const c = new Militar (f_nome.value, f_portas.value, f_blindagem.value, f_municao.value)
+            a_carros.push(c)
+        }
+        gerenciarExibicaoCarros()
+})
 })
 
 class Carro { // Classe pai
@@ -85,25 +100,3 @@ class Militar extends Carro { // Classe filho
         }
     }
 }
-
-const c1 = new Carro("Normal", 4) 
-c1.ligar()
-c1.setCor("Preto")
-
-const c2 = new militar("Lutador", 1, 100, 50)
-// c2.setCor = ("Azul") // Pode modificar a cor quando quiser
-
-console.log(`Nome: ${c1.nome}`)
-console.log(`Portas: ${c1.portas}`)
-console.log(`Ligado: ${(c1.ligado? "Sim" : "Não")}`)
-console.log(`Velocidade: ${c1.vel}`)
-console.log(`Cor: ${c1.cor}`)
-console.log("---------------------------------")
-console.log(`Nome: ${c2.nome}`)
-console.log(`Portas: ${c2.portas}`)
-console.log(`Ligado: ${(c2.ligado? "Sim" : "Não")}`)
-console.log(`Velocidade: ${c2.vel}`)
-console.log(`Blindagem: ${c2.blindagem}`)
-console.log(`Munição: ${c2.municao}`)
-console.log(`Cor: ${c2.cor}`)
-console.log("---------------------------------")
