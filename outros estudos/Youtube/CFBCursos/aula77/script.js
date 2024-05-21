e70f770e12acf5411297d66cfd51b30622460f24
TawanDeodato <tawandeodato94@gmail.com>
2024-05-21 20:48:00 -0300

    Fazendo um timer no JavaScript

--- outros estudos/Youtube/CFBCursos/aula77/script.js
+++ outros estudos/Youtube/CFBCursos/aula77/script.js
@@ -2,6 +2,33 @@
 
 const div_data = document.getElementById("div_data")
 const div_relogio = document.getElementById("div_relogio")
+const btn_ativar = document.getElementById("btn_ativar")
+const btn_parar = document.getElementById("btn_parar")
+const tmp_alarme = document.getElementById("tmp_alarme")
+const hora_alarme = document.getElementById("hora_alarme")
+const timer = document.getElementById("timer")
+
+let ts_atual = null
+let ts_alarme = null
+let alarme_ativado = false
+let alarme_tocando = false
+
+btn_ativar.addEventListener("click", () => {
+    ts_atual = Date.now()
+    ts_alarme = ts_atual + (tmp_alarme.value*1000)
+    alarme_ativado = true
+    const dt_alarme = new Date(ts_alarme)
+    hora_alarme.innerHTML = "Hora do Alarme: " + dt_alarme.getHours() + ":" + dt_alarme.getMinutes() + ":" + dt_alarme.getSeconds()
+})
+
+btn_parar.addEventListener("click", () => {
+    alarme_ativado = false
+    alarme_tocando = false
+    hora_alarme.innerHTML = "Hora do alarme: "
+    tmp_alarme.value = 0
+    timer.classList.remove("alarme")
+})
+
 const data = new Date ()
 
 //  console.log(Date.now())  // Timestamp
@@ -17,6 +44,28 @@ data_mes = data_mes < 10 ? "0" + data_mes : data_mes
 const data_r = data_dia + "/" + data_mes + "/" + data.getFullYear()
 
 div_data.innerHTML= data_r
+
+
+const relogio = () => {
+    const data = new Date()
+    let hora = data.getHours()
+    let minuto = data.getMinutes()
+    let segundos = data.getSeconds()
+    hora = hora < 10 ? "0" + hora : hora
+    minuto = minuto < 10 ? "0" + minuto : minuto
+    segundos = segundos < 10 ? "0" + segundos : segundos
+    const hora_completa = hora + ":" + minuto + ":" + segundos
+    div_relogio.innerHTML = hora_completa
+    if (alarme_ativado && !alarme_tocando) {
+        if(data.getTime() >= ts_alarme) {
+            alarme_tocando = true
+            timer.classList.add("alarme")
+        }
+    }
+}
+
+const intervalo = setInterval(relogio, 1000)
+
 
 // getDate() = Dia do mês
 // getDay() = Dia da semana (número)
