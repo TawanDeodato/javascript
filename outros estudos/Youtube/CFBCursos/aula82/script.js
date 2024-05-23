d4c00a90582ed6d07053f5c6e7f516a418f4426f
TawanDeodato <tawandeodato94@gmail.com>
2024-05-23 12:24:53 -0300

    Atualização do projeto de bolinhas

--- outros estudos/Youtube/CFBCursos/aula82/script.js
+++ outros estudos/Youtube/CFBCursos/aula82/script.js
@@ -13,17 +13,47 @@ let bolas = []
 let numBola = 0
 
 class Bola {
-    constructor() {
-        this.size = Math.floor(Math.random() * 15)+10
+    constructor(arrayBolas, palco) {
+        this.size = Math.floor(Math.random()*15)+10
         // Definindo as cores aleatoriamente 
-        this.r = Math.floor(Math.random() * 255)
-        this.g = Math.floor(Math.random() * 255)
-        this.b = Math.floor(Math.random() * 255)
+        this.r = Math.floor(Math.random()*255)
+        this.g = Math.floor(Math.random()*255)
+        this.b = Math.floor(Math.random()*255)
         // Definindo as posições aleatoriamente
         this.px = Math.floor(Math.random()*(larguraPalco - this.size))
         this.py = Math.floor(Math.random()*(alturaPalco - this.size))
+        // Definindo velocidades diferentes
+        this.velx = Math.floor(Math.random()*2)+0.5
+        this.vely = Math.floor(Math.random()*2)+0.5
+        // Definindo direções aleatoriamente
+        this.dirx = (Math.random()*10) > 5 ? 1 : -1
+        this.diry = (Math.random()*10) > 5 ? 1 : -1
+        // Definindo o local onde vão sair as bolinhas
+        this.palco = palco
+        this.arrayBolas = arrayBolas
+        // Definindo id diferentes para cada bolinhas
+        this.id = Date.now() + "_" + Math.floor(Math.random()*100000000000000)
+        this.desenhar()
+        // Definindo o controle das bolinhas
+        this.controle = setInterval(this.controlar, 10)
+        // Relacionando uma bolinha criada com a bolinha do array "bolas"
+        this.eu = document.getElementById(this.id)
+    }
+    minhaPos = () => {
+
     }
 
+    remover = () => {
+
+    }
+
+    desenhar = () => {
+
+    }
+
+    controlar = () => {
+
+    }
 }
 
 window.addEventListener("resize", (e) => {
