3a26559ceb9b82eb7a6ea06352a5231dc824bc96
TawanDeodato <tawandeodato94@gmail.com>
2024-05-23 20:37:30 -0300

    Estudos sobre navegação entre páginas

---
+++ outros estudos/Youtube/CFBCursos/aula87/script.js
@@ -0,0 +1,13 @@
+const url = document.getElementById("url")
+const btn_url = document.getElementById("btn_url")
+
+btn_url.addEventListener("click", (evt) => {
+   // window.location = "https://www.google.com.br"
+
+   // window.location = "https://www.google.com.br" // Deleta a URl corrente do histórico 
+
+    // window.assing = "https://www.google.com.br" // Não deleta a URl corrente do histórico
+
+    // window.location.reload() // Faz o recarregamento da página
+
+})
