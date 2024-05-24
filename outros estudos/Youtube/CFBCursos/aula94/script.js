4d37973dcdd1853f9967781f2c789fbf7ace7dc8
TawanDeodato <tawandeodato94@gmail.com>
2024-05-24 13:50:26 -0300

    estudos sobre módulos em javascript parte 2

---
+++ outros estudos/Youtube/CFBCursos/aula94/script.js
@@ -0,0 +1,7 @@
+// import getTodosCursos, {cursos} from "./cursos.js"
+import getTodosCursos from "./cursos.js"
+// import {cursos as c} from "./cursos.js"
+import * as m_cursos from "./cursos.js"
+
+console.log(m_cursos.cursos[2]) 
+console.log(getTodosCursos()) 
