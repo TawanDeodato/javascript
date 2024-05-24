88ac6ff8557593088392980bf5e83681b99edf0c
TawanDeodato <tawandeodato94@gmail.com>
2024-05-24 13:07:33 -0300

    estudos sobre módulos em javascript parte 2

---
+++ outros estudos/Youtube/CFBCursos/aula93/cursos.js
@@ -0,0 +1,8 @@
+const cursos = ["JavaScript", "HTML", "CSS", "Arduino", "Raspberry", "C++", "Python", "C#"]
+
+const getTodosCursos = () => {
+    return cursos
+}
+
+export {cursos}
+export default getTodosCursos
---
+++ outros estudos/Youtube/CFBCursos/aula93/script.js
@@ -0,0 +1,6 @@
+// import getTodosCursos, {cursos} from "./cursos.js"
+import getTodosCursos from "./cursos.js"
+import {cursos} from "./cursos.js"
+
+console.log(cursos) 
+console.log(getTodosCursos()) 
