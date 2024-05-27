77330b5391c03827cfb2ee49935eaf52c2267b8f
TawanDeodato <tawandeodato94@gmail.com>
2024-05-27 12:25:43 -0300

    estudos sobre a classe String no JavaScript

---
+++ outros estudos/Youtube/CFBCursos/aula101/script.js
@@ -0,0 +1,13 @@
+let nome = new String("Tawan") 
+let curso = new String("JavaScript") 
+
+console.log(nome)
+console.log(typeof(nome))
+
+
+console.log(nome.charAt(0)) // Retorna o valor dentro da classe que correponde ao indice indicado
+console.log(nome.charAt(1))
+
+console.log(nome.charCodeAt(0)) // Retorna o código do caractere
+
+console.log(nome.concat(` ${curso}`)) // Concatena uma string com outra
