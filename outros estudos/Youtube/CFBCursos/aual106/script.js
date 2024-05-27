bfcc40672ba88f7383e78522fd6e84c7621d376d
TawanDeodato <tawandeodato94@gmail.com>
2024-05-27 14:02:52 -0300

    Estudos sobre RegExp em JavaScript (parte 2)

---
+++ outros estudos/Youtube/CFBCursos/aual106/script.js
@@ -0,0 +1,12 @@
+// Estudos sobre RegExp em JavaScript:
+
+let nome = new String("Tawan estuda JavaSript")
+
+console.log(nome)
+
+console.log(nome.search(/Estuda/i))
+
+console.log(nome.match(/A/ig))
+
+// console.log(nome.replace(/Estuda/i, "Estudou"))
+console.log(nome.replace(/a/ig, "A"))
--- outros estudos/Youtube/CFBCursos/aula105/script.js
+++ outros estudos/Youtube/CFBCursos/aula105/script.js
@@ -1,12 +1,20 @@
-// Estudos sobre RegExp em JavaScript:
+// Estudos sobre RegExp em JavaScript (parte 2):
 
-let nome = new String("Tawan estuda JavaSript")
+let nome = new String("Tawaaaaaaaan estudaaaaaaaa JavaScript 1985")
+let emial = "tawan@tawan.com.br"
+let numeros = "1, 10, 100, 1000"
 
-console.log(nome)
+console.log(nome.match(/[a-m | 0-7]/ig))
 
-console.log(nome.search(/Estuda/i))
+// Metacaracteres
+console.log(nome.match(/\d/ig))
+console.log(nome.match(/\s/ig))
+console.log(nome.match(/\bJ/ig))
 
-console.log(nome.match(/A/ig))
-
-// console.log(nome.replace(/Estuda/i, "Estudou"))
-console.log(nome.replace(/a/ig, "A"))
+//Quantificadores
+console.log(nome.match(/a+/ig))
+console.log(nome.match(/wa+/ig))
+console.log(numeros.match(/10/ig))
+console.log(numeros.match(/10+/ig))
+console.log(numeros.match(/10*/ig))
+console.log(numeros.match(/10?/ig))
