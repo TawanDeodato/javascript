fdd3ac9e85a88c1f43ecbfc2ddafc2a1de3231de
TawanDeodato <tawandeodato94@gmail.com>
2024-05-31 12:37:09 -0300

    Erro corrigido

--- outros estudos/Youtube/proj-api/js/script.js
+++ outros estudos/Youtube/proj-api/js/script.js
@@ -22,16 +22,18 @@ async function getAllPosts() {
     console.log(data)
 
     loadingElement.classList.add("hide")
+
     data.map((post) => {
         const div = document.createElement("div")
         const title = document.createElement("h2")
         const body = document.createElement("p")
         const link = document.createElement("a")
 
-        title.innerText = post.title
-        body.innerText = post.body
-        link.innerText = "Ler"
-        link.setAttribute("href", `/post.html?id=${post.id}`)       
+        title.innerText = post.title;
+        body.innerText = post.body;
+        link.innerText = "Ler";
+        link.setAttribute("href", `post.html?id=${post.id}`)
+              
 
         div.appendChild(title)
         div.appendChild(body)
@@ -84,7 +86,7 @@ function createComment(comment) {
 }
 
 if (!postId) {
-    getAllPosts()
+    getAllPosts();
 } else {
-    console.log(postId)
+    getPost(postId)
 } 
