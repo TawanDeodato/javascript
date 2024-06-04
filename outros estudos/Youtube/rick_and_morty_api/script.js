7046e8b4a9cf3df2073ba69d319465367a4f27a3
TawanDeodato <tawandeodato94@gmail.com>
2024-06-04 13:47:29 -0300

    Filtrando a api

--- outros estudos/Youtube/rick_and_morty_api/script.js
+++ outros estudos/Youtube/rick_and_morty_api/script.js
@@ -1,5 +1,5 @@
 const characterId = document.getElementById("characterId")
-const btn_go = document.getElementById("btn_go")
+const btn_pesq = document.getElementById("btn_pesq")
 const content = document.getElementById("content")
 const img = document.getElementById("img")
 
@@ -14,9 +14,22 @@ const fetchApi = (value) => {
     return url
 }
 
-btn_go.addEventListener("click", async (evt) => {
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode']
+
+const buildResult = (result) => {
+    const newObject = {};
+    keys.map((key) => document.getElementById(key))
+      .map((elem) => {
+        elem.checked && (newObject[elem.name] = result[elem.name])
+      })
+      return newObject
+}
+
+btn_pesq.addEventListener("click", async (evt) => {
     evt.preventDefault()
     const result = await fetchApi(characterId.value)
-    content.textContent = `${JSON.stringify(result, undefined, 2)}`
+    // content.textContent = `${JSON.stringify(result, undefined, 2)}`
+    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`
+    console.log(buildResult(result))
     img.src = `${result.image}`
 })
