28ce4dd2cb8798024f5b64b8930206e575e13211
TawanDeodato <tawandeodato94@gmail.com>
2024-06-05 13:33:50 -0300

    Manipulando o DOM do projeto

--- outros estudos/Youtube/rick_and_morty_api/script.js
+++ outros estudos/Youtube/rick_and_morty_api/script.js
@@ -1,35 +1,69 @@
 const characterId = document.getElementById("characterId")
 const btn_pesq = document.getElementById("btn_pesq")
+const btn_limpar = document.getElementById("btn_limpar")
 const content = document.getElementById("content")
+const conteinerResult = document.getElementById("result_style")
 const img = document.getElementById("img")
 
 const fetchApi = (value) => {
     const url = fetch(`https://rickandmortyapi.com/api/character/${value}`)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
         return data
     })
 
     return url
 }
 
-const keys = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode']
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode']
+const newKeys = {
+    name: "Nome",
+    status: "Status",
+    species: "Espécie",
+    gender: "Gênero",
+    origin: "Planeta de Origem",
+    episode: "Episódios"
+}
 
 const buildResult = (result) => {
-    const newObject = {};
-    keys.map((key) => document.getElementById(key))
+    return keys.map((key) => document.getElementById(key))
       .map((elem) => {
-        elem.checked && (newObject[elem.name] = result[elem.name])
+        if(elem.checked === true && (Array.isArray(result[elem.name])) === true) {
+            const arrayResult = result[elem.name].join('\r\n')
+            const newElem = document.createElement("p");
+            newElem.innerHTML = `${newKeys[elem.name]} : ${arrayResult}`;
+            content.appendChild(newElem)
+        } else if(elem.checked === true && (elem.name === 'origin')) {
+            const newElem = document.createElement("p");
+            newElem.innerHTML = `${newKeys[elem.name]} : ${result[elem.name].name}`;
+            content.appendChild(newElem)
+        } else if(elem.checked === true && typeof(result[elem.name]) !== 'object') {
+            const newElem = document.createElement("p");
+            newElem.innerHTML = `${newKeys[elem.name]} : ${result[elem.name]}`;
+            content.appendChild(newElem)
+        } 
       })
-      return newObject
 }
 
 btn_pesq.addEventListener("click", async (evt) => {
-    evt.preventDefault()
-    const result = await fetchApi(characterId.value)
-    // content.textContent = `${JSON.stringify(result, undefined, 2)}`
-    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`
-    console.log(buildResult(result))
-    img.src = `${result.image}`
+    evt.preventDefault();
+
+    if(characterId.value === '') {
+        img.src = ""
+        return content.innerHTML = "Coloca um número ali para pesquisar antes né? seu animal!"
+    }
+
+    const result = await fetchApi(characterId.value);
+    if(content.firstChild === null) {
+        conteinerResult.className = "result_style"
+        img.src = `${result.image}`; 
+        buildResult(result);
+    } else {
+        content.innerHTML = ""
+        conteinerResult.className = "result_style"
+        img.src = `${result.image}`; 
+        buildResult(result);
+    }
 })
+
+btn_limpar.addEventListener("click", () => location.reload())
